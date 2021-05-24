8e4a28271a96638819aebcaff099e2b0c0d000c0
danaratagayeva <danara.tagayeva@gmail.com>
2021-05-24 19:38:37 -0400

    project4 calc created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+  const [calc, setCounter]=useState(10);
+
+    const plus=()=>{
+      setCounter(calc+1)
+    }
+    const minus=()=>{
+      setCounter(calc-1)
+    }
+    const multiple=()=>{
+      setCounter(calc*2)
+    }
+    const divide=()=>{
+      setCounter(calc/3)
+    }        
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Calculator:{calc}</h1>
+      <button onClick={plus}>+</button>
+      <button onClick={minus}>-</button>
+      <button onClick={multiple}>*2</button>
+      <button onClick={divide}>/3</button>    
     </div>
+
+
   );
 }
 
